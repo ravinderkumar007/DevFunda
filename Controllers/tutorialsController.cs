dc50a8d046b3aa2ac8c6b9c2a2ceb2dd37242819
Ravinder kumar <ravinderkumard@gmail.com>
2025-06-26 13:49:14 +0530

    Adding of mongodb tutorial

---
+++ Controllers/tutorialsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Devfunda.Controllers
+{
+    public class tutorialsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MongoDBtutorial()
+        {
+            return View();
+        }
+    }
+}
