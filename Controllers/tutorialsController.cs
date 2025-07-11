9b837fae758867721e63a851336cb5afecbf230b
Ravinder kumar <ravinderkumard@gmail.com>
2025-07-11 08:59:55 +0530

    Adding of Tutorial menu

--- Controllers/tutorialsController.cs
+++ Controllers/tutorialsController.cs
@@ -4,6 +4,13 @@ namespace Devfunda.Controllers
 {
     public class tutorialsController : Controller
     {
+        private static readonly Dictionary<string, string> SlugToViewMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+    {
+        { "node-js-intro", "Intro" },
+        { "what-is-node-js", "whatisnode" },
+        { "node-js-middleware", "Middleware" },
+        // Add more as needed
+    };
         public IActionResult Index()
         {
             return View();
@@ -12,5 +19,22 @@ namespace Devfunda.Controllers
         {
             return View();
         }
+        public IActionResult NodeJs()
+        {
+            return View();
+        }
+        [Route("tutorials/nodejs/{topic}")]
+        public IActionResult NodeJs(string topic)
+        {
+
+            if (SlugToViewMap.TryGetValue(topic, out var viewName))
+            {
+                return View("nodejs/"+viewName); // Opens Views/Tutorials/Nodejs/Intro.cshtml etc.
+            }
+
+            return NotFound(); // Or redirect to a default page
+        }
     }
 }
+
+   
